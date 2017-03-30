6029badd2f02d9e8eebe81cdcbbdf2c985592f3b
Simon Groenborg <groenborg27@gmail.com>
2017-03-30 14:10:53 +0200

    updated build script

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e -u -x
 
-mkdir -p assets
+go get -d -t -v ./...
 
 export GOOS=linux
 export GOARCH=amd64
 
-go build -o assets/check check/check.go
-go build -o assets/in in/in.go
-go build -o assets/out out/out.go
+go build -o concourse-git-phlow/assets/check concourse-git-phlow/check/check.go
+go build -o concourse-git-phlow/assets/in concourse-git-phlow/in/in.go
+go build -o concourse-git-phlow/assets/out concourse-git-phlow/out/out.go
 
-chmod +x assets/check
-chmod +x assets/in
-chmod +x assets/out
+chmod +x concourse-git-phlow/assets/check
+chmod +x concourse-git-phlow/assets/in
+chmod +x concourse-git-phlow/assets/out
 
-ls
+cp -R concourse-git-phlow/* concourse-git-phlow-artifacts/
