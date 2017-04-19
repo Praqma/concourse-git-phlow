a25b3f4cb6985e13135d17b41a507be0b34faa2e
Simon Groenborg <groenborg27@gmail.com>
2017-04-19 10:20:07 +0200

    updated godep

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e -u -x
 
-go get -d -t -v ./...
+godep go get -d -t -v ./...
 
 export GOOS=linux
 export GOARCH=amd64
 
-go build -o concourse-git-phlow/assets/check concourse-git-phlow/check/check.go
-go build -o concourse-git-phlow/assets/in concourse-git-phlow/in/in.go
-go build -o concourse-git-phlow/assets/out concourse-git-phlow/out/out.go
+godep go build -o concourse-git-phlow/assets/check concourse-git-phlow/check/check.go
+godep go build -o concourse-git-phlow/assets/in concourse-git-phlow/in/in.go
+godep go build -o concourse-git-phlow/assets/out concourse-git-phlow/out/out.go
 
 chmod +x concourse-git-phlow/assets/check
 chmod +x concourse-git-phlow/assets/in
 chmod +x concourse-git-phlow/assets/out
 
-cp -R concourse-git-phlow/* concourse-git-phlow-artifacts/
+#cp -R concourse-git-phlow/* concourse-git-phlow-artifacts/
